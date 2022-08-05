391ec3a715b1b81be15f1ba3b06854de58451e8a
mwatanabe <mwatanabe@lvn.co.jp>
2022-08-05 14:48:18 +0900

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,4 +14,7 @@ final class Controller extends BaseController
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+
+     // 以下を追記します
+     public function csFixerError(): string{$hoge = 'hoge';$huga = 'huga';return $hoge . $huga;}
 }

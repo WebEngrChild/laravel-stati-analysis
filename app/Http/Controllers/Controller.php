d9ef2696988e3685d430233aa466f67a84fb26d1
mwatanabe <mwatanabe@lvn.co.jp>
2022-08-05 12:18:37 +0900

    bad code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,4 +15,7 @@ final class Controller extends BaseController
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+
+    // 動作検証
+    public function csFixerError(): string{$hoge = 'hoge';$huga = 'huga';return $hoge . $huga;}
 }
